da517f9396ebc23a132edfc4fb5d2dcf5246e260
Santiago Florez <santiagoflorezv23@gmail.com>
2024-03-21 19:59:28 -0500

    dashboard

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const hbs = require('hbs');
+const port = 3000
+
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials', function (err) {console.log("error al implementar vista parcial")});
+
+app.use(express.static('public'))
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname+'/public/dashboard/examples/dashboard.html')
+// })
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/categorias', (req, res) => {
+    res.render(__dirname+'/views/categorias.hbs')
+  })
+
+  app.get('/productos', (req, res) => {
+    res.render(__dirname+'/views/productos.hbs')
+  })
+
+  app.get('/insumos', (req, res) => {
+    res.render(__dirname+'/views/insumos.hbs')
+  })
+app.listen(port, () => {
+  console.log(`Ejecucion en el puerto http://localhost:${port}`)
+})
